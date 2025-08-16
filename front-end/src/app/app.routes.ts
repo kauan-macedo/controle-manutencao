c533b8613fd9ad4ab55c1be42cc92f24474b318c
Enzo Spironelli <enzo@localhost.localdomain>
2025-08-16 02:44:55 -0300

    criando componente de login e comecando a configurar rotas

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login'
+import { Layout } from './pages/layout/layout'
+import { Dashboard } from './pages/dashboard/dashboard'
 
-export const routes: Routes = [];
+
+
+/*
+
+definindo a rota de login como default por enquanto, os componentes que vêm após a página de login podem ser 
+criados dentro do componente layout (assim como dashboard, até agora)
+
+*/
+
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+
+
+    {
+        path:'login',
+        component:Login
+    },
+
+
+    {
+        path:'',
+        component: Layout,
+
+        //criar componentes novos aqui
+        children: [
+            {
+                path: 'dashboard',
+                component: Dashboard
+            }
+        ]
+    }
+];
