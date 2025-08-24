e4e411d44f52c0a58a392fc868d6e0d3c382a403
Spirou1 <enzospironellipinheiro@gmail.com>
2025-08-24 18:29:14 -0300

    implementando integracao com api do viaCEP

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    //IMPORTANTE! CONFIGURANDO O CLIENTE HTTP PARA USAR A API FETCH AO INVÉS DE XMLHTTPREQUEST
+    provideHttpClient(
+      withFetch(),
+    ),
   ]
 };
--- front-end/src/app/pages/autocadastro/autocadastro.ts
+++ front-end/src/app/pages/autocadastro/autocadastro.ts
@@ -1,42 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-autocadastro',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, HttpClientModule],
   templateUrl: './autocadastro.html',
   styleUrl: './autocadastro.css'
 })
 export class Autocadastro implements OnInit {
   cadastroForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.cadastroForm = this.fb.group({});
+  constructor(private http: HttpClient) {
+    this.cadastroForm = new FormGroup({});
   }
 
+  // inicializando controles do formulário quando componente for criado
   ngOnInit(): void {
-    this.cadastroForm = this.fb.group({
-      nome: [''],
-      email: [''],
-      cpf: [''],
-      telefone: [''],
-      cep: [''],
-      logradouro: [''],
-      bairro: [''],
-      cidade: [''],
-      estado: [''],
-      complemento: ['']
+    this.cadastroForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      cpf: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      cep: new FormControl('', Validators.required),
+      logradouro: new FormControl('', Validators.required),
+      bairro: new FormControl('', Validators.required),
+      cidade: new FormControl('', Validators.required),
+      estado: new FormControl('', Validators.required),
+      complemento: new FormControl('')
     });
   }
 
-  onSubmit() {
-    // implementar a lógica dos dados
+  // função para completar automaticamente o endereço usando a api ViaCEP
+  buscarEndereco(): void {
+    const cep = this.cadastroForm.get('cep')?.value;
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Erro na rede ou no servidor');
+          }
+          return response.json();
+        })
+        .then(dados => {
+          this.cadastroForm.patchValue({
+            logradouro: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+          });
+        })
+    }
+
   }
 
-  buscarEndereco() {
-    // implementar função que chama a api viaCep para completar automaticamente os campos de endereço
+   onSubmit(): void {
+    //implementar método onsubmit de cadastro!
   }
 }
