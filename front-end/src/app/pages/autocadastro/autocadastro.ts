63538be6a60acdbb7e6c760f57ca2d2aab3a4d4c
Spirou1 <enzospironellipinheiro@gmail.com>
2025-08-24 17:14:06 -0300

    criando template da pagina de autocadastro

--- front-end/src/app/pages/autocadastro/autocadastro.ts
+++ front-end/src/app/pages/autocadastro/autocadastro.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-autocadastro',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './autocadastro.html',
   styleUrl: './autocadastro.css'
 })
-export class Autocadastro {
+export class Autocadastro implements OnInit {
+  cadastroForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({});
+  }
+
+  ngOnInit(): void {
+    this.cadastroForm = this.fb.group({
+      nome: [''],
+      email: [''],
+      cpf: [''],
+      telefone: [''],
+      cep: [''],
+      logradouro: [''],
+      bairro: [''],
+      cidade: [''],
+      estado: [''],
+      complemento: ['']
+    });
+  }
+
+  onSubmit() {
+    // implementar a lógica dos dados
+  }
+
+  buscarEndereco() {
+    // implementar função que chama a api viaCep para completar automaticamente os campos de endereço
+  }
 }
