6738afd1e898a926a803f55af5e4214bcf2e79af
Kauan Macedo <kauanmacedo85@outlook.com>
2025-08-24 20:21:39 -0300

    cliente-dashboard (dados hardcoded)

--- front-end/src/app/pages/layout/layout.ts
+++ front-end/src/app/pages/layout/layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
