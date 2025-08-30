c7b3f0d8179c73c29429ad7ea6b4e661f9c83bfa
Spirou1 <enzospironellipinheiro@gmail.com>
2025-08-30 03:04:02 -0300

    criando formulario de criacao de solicitacao e adicionando modal na pagina inicial

--- front-end/src/app/pages/cliente/cliente-criar-solicitacao/cliente-criar-solicitacao.ts
+++ front-end/src/app/pages/cliente/cliente-criar-solicitacao/cliente-criar-solicitacao.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-criar-solicitacao',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './cliente-criar-solicitacao.html',
   styleUrl: './cliente-criar-solicitacao.css'
 })
-export class ClienteCriarSolicitacao {
+export class ClienteCriarSolicitacao implements OnInit {
 
+  solicitacao = {
+    descricaoEquipamento: '',
+    categoriaEquipamento: '',
+    descricaoDefeito: ''
+  };
+
+  // lista de categorias para o combo box
+  categorias: string[] = ['Notebook', 'Desktop', 'Impressora', 'Mouse', 'Teclado'];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: any): void {
+    //implementar logica
+  }
 }
--- front-end/src/app/pages/cliente/cliente-pagina-inicial/cliente-pagina-inicial.ts
+++ front-end/src/app/pages/cliente/cliente-pagina-inicial/cliente-pagina-inicial.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClienteCriarSolicitacao } from '../cliente-criar-solicitacao/cliente-criar-solicitacao';
+
 
 @Component({
   selector: 'app-cliente-pagina-inicial',
-  imports: [],
+  imports: [ ClienteCriarSolicitacao, CommonModule ],
   templateUrl: './cliente-pagina-inicial.html',
   styleUrl: './cliente-pagina-inicial.css'
 })
+
 export class ClientePaginaInicial {
 
+  //adicionando funcao para exibir o componente de criar solicitacao na pagina inicial
+  exibirModal: boolean = false;
+
+  abrirModal(): void {
+    this.exibirModal = true;
+  }
+
+  fecharModal(): void {
+    this.exibirModal = false;
+  }
 }
