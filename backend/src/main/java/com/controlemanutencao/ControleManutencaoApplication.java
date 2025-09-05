1ff3429cd8ab55ab2160fd62b452ecbb977f70d8
victorbetoni <victorbetoni@protonmail.com>
2025-09-05 12:13:03 -0300

    Implementando serviço de email

--- backend/src/main/java/com/controlemanutencao/ControleManutencaoApplication.java
+++ backend/src/main/java/com/controlemanutencao/ControleManutencaoApplication.java
@@ -1,5 +1,6 @@
 package com.controlemanutencao;
 
+import com.controlemanutencao.service.MailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ControleManutencaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControleManutencaoApplication.class, args);
+        SpringApplication.run(ControleManutencaoApplication.class, args);
 	}
 
 }
--- backend/src/main/java/com/controlemanutencao/service/MailService.java
+++ backend/src/main/java/com/controlemanutencao/service/MailService.java
@@ -1,4 +1,48 @@
 package com.controlemanutencao.service;
 
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
 public class MailService {
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    public void sendEmail(String to, String title, String msg) throws Exception {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("controlemanutencao984@gmail.com", "qfcj fmjc lpci odbr");
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("controlemanutencao984@gmail.com"));
+        message.setRecipients(
+                Message.RecipientType.TO,
+                InternetAddress.parse(to)
+        );
+        message.setSubject(title);
+        message.setText(msg);
+
+        Transport.send(message);
+
+    }
+
+
+
 }
