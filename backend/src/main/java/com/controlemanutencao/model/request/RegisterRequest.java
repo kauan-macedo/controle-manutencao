3ef60f4a83c13ee38f464990ad0a27f3ca73b51b
victorbetoni <victorbetoni@protonmail.com>
2025-09-05 13:40:40 -0300

    Adicionando campo de número no autocadastro

--- backend/src/main/java/com/controlemanutencao/model/request/RegisterRequest.java
+++ backend/src/main/java/com/controlemanutencao/model/request/RegisterRequest.java
@@ -1,3 +1,14 @@
 package com.controlemanutencao.model.request;
 
-public record RegisterRequest(String email) { }
+public record RegisterRequest(
+    String email,
+    String nome,
+    String cpf,
+    String telefone,
+    String rua,
+    String cep,
+    int numero,
+    String bairro,
+    String cidade,
+    String estado
+) { }
