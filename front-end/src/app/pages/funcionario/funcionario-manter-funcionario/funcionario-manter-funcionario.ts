9383e8c4ef6698d5eef947fec411039ba6959dbe
Kauan Macedo <kauanmacedo85@outlook.com>
2025-09-13 15:10:57 -0300

    CRUD funcionario

--- front-end/src/app/pages/funcionario/funcionario-manter-funcionario/funcionario-manter-funcionario.ts
+++ front-end/src/app/pages/funcionario/funcionario-manter-funcionario/funcionario-manter-funcionario.ts
@@ -10,31 +10,67 @@ import { CommonModule } from '@angular/common';
   styleUrl: './funcionario-manter-funcionario.css'
 })
 export class FuncionarioManterFuncionario implements OnInit {
-  funcionarios: any[] = [
-    { id: 1, nome: 'Maria', email: 'maria@exemplo.com' },
-    { id: 2, nome: 'Mário', email: 'mario@exemplo.com' },
-  ];
+  funcionarios: any[] = [];
 
   novoFuncionario = { nome: '', email: '', senha: '' };
   funcionarioEmEdicao: any = null;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.carregarDoLocalStorage();
+  }
+  private carregarDoLocalStorage(): void {
+    const dados = localStorage.getItem('funcionarios');
+    if (dados) {
+      this.funcionarios = JSON.parse(dados);
+    } else {
+      this.funcionarios = [
+        { id: 1, nome: 'Maria', email: 'maria@exemplo.com' },
+        { id: 2, nome: 'Mário', email: 'mario@exemplo.com' },
+      ];
+      this.salvarNoLocalStorage();
+    }
+  }
+
+  private salvarNoLocalStorage(): void {
+    localStorage.setItem('funcionarios', JSON.stringify(this.funcionarios));
+  }
 
   onAdicionar(form: any): void {
-    
+    if (form.invalid) {
+      return;
+
+    }
+
+    const novoId = new Date().getTime();
+    this.funcionarios.push({id: novoId, ...this.novoFuncionario});
+    this.salvarNoLocalStorage();
+    form.resetForm();
   }
 
   onEditar(funcionario: any): void {
-    
+    this.funcionarioEmEdicao = { ...funcionario };
   }
 
   onSalvarEdicao(): void {
-    
+    if (!this.funcionarioEmEdicao) {
+      return;
+    }
+
+    const index = this.funcionarios.findIndex(f => f.id === this.funcionarioEmEdicao.id);
+    if (index !== -1) {
+
+      this.funcionarios[index] = this.funcionarioEmEdicao;
+      this.salvarNoLocalStorage();
+    }
+
+
+    this.funcionarioEmEdicao = null;
   }
 
   onRemover(id: number): void {
-    
+    this.funcionarios = this.funcionarios.filter(f => f.id !== id);
+    this.salvarNoLocalStorage();
   }
 }
