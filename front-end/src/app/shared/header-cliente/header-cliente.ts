4b0db11feb6ec625398f864ab4486d5906746483
Spirou1 <enzospironellipinheiro@gmail.com>
2025-09-13 17:04:28 -0300

    implementando dark mode para as paginas sem perfil e perfil de cliente

--- front-end/src/app/shared/header-cliente/header-cliente.ts
+++ front-end/src/app/shared/header-cliente/header-cliente.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle';
 
 @Component({
   selector: 'app-header-cliente',
-  imports: [RouterLink],
+  imports: [RouterLink, ThemeToggleComponent],
   templateUrl: './header-cliente.html',
   styleUrl: './header-cliente.css'
 })
--- front-end/src/app/shared/header-funcionario/header-funcionario.ts
+++ front-end/src/app/shared/header-funcionario/header-funcionario.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle';
 
 @Component({
   selector: 'app-header-funcionario',
-  imports: [RouterLink],
+  imports: [RouterLink, ThemeToggleComponent],
   templateUrl: './header-funcionario.html',
   styleUrl: './header-funcionario.css'
 })
