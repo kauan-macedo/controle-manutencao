c6760390a7ca4b3ed1690cdaf339eff78f60e139
Spirou1 <enzospironellipinheiro@gmail.com>
2025-09-16 20:38:09 -0300

    adicionando modelo de usuario e endereco

---
+++ front-end/src/app/models/endereco.ts
@@ -0,0 +1,8 @@
+export type Endereco = {
+    cep: string,
+    logradouro: string,
+    bairro: string,
+    cidade: string,
+    estado: string,
+    numero: string
+};
--- front-end/src/app/models/usuario.ts
+++ front-end/src/app/models/usuario.ts
@@ -1,21 +1,21 @@
-
+import { Endereco } from './endereco';
 export class Usuario {
-    id: number;
-    nome: string;
-    email: string;
-    cpf: string;
-    telefone: string;
-    perfil: string;
-    senha: string;
+  id: number;
+  nome: string;
+  email: string;
+  cpf: string;
+  telefone: string;
+  perfil: string;
+  senha: string;
+  endereco: Endereco;
 
-
-constructor(
+  constructor(
     nome: string,
     email: string,
     cpf: string,
     telefone: string,
     perfil: string = 'CLIENTE'
-) {
+  ) {
     this.id = 0;
     this.nome = nome;
     this.email = email;
@@ -23,5 +23,13 @@ constructor(
     this.telefone = telefone;
     this.perfil = perfil;
     this.senha = '';
-    }
+    this.endereco = {
+      cep: '',
+      logradouro: '',
+      bairro: '',
+      cidade: '',
+      estado: '',
+      numero: '',
+    };
+  }
 }
