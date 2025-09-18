ca88f7ef564f79f255070452ca4f42579cda063e
Spirou1 <enzospironellipinheiro@gmail.com>
2025-09-17 21:52:33 -0300

    usando fontawesome para adicionar um icone aos headers

--- front-end/src/app/shared/header-cliente/header-cliente.ts
+++ front-end/src/app/shared/header-cliente/header-cliente.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
+import { faComputer } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @Component({
   selector: 'app-header-cliente',
-  imports: [RouterLink, ThemeToggle],
+  imports: [RouterLink, ThemeToggle, FontAwesomeModule],
   templateUrl: './header-cliente.html',
   styleUrl: './header-cliente.css'
 })
 export class HeaderCliente {
-
+  faComputer = faComputer; //ícone que será mostrado no header
 }
--- front-end/src/app/shared/header-funcionario/header-funcionario.ts
+++ front-end/src/app/shared/header-funcionario/header-funcionario.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
+import { faComputer } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-header-funcionario',
-  imports: [RouterLink, ThemeToggle],
+  imports: [RouterLink, ThemeToggle, FontAwesomeModule],
   templateUrl: './header-funcionario.html',
   styleUrl: './header-funcionario.css'
 })
 export class HeaderFuncionario {
-
+  faComputer = faComputer; //ícone que será mostrado no header
 }
