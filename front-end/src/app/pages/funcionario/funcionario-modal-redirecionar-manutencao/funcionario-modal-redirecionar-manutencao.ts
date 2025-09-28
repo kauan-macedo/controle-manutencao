2e0d8653dc640d0b7c1714c043e8f009890f1a11
Kauan Macedo <kauanmacedo85@outlook.com>
2025-09-28 13:17:11 -0300

    Modal de redirecionar manutenção

--- front-end/src/app/pages/funcionario/funcionario-modal-redirecionar-manutencao/funcionario-modal-redirecionar-manutencao.ts
+++ front-end/src/app/pages/funcionario/funcionario-modal-redirecionar-manutencao/funcionario-modal-redirecionar-manutencao.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-funcionario-modal-redirecionar-manutencao',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './funcionario-modal-redirecionar-manutencao.html',
   styleUrl: './funcionario-modal-redirecionar-manutencao.css'
 })
-export class FuncionarioModalRedirecionarManutencao {
+export class FuncionarioModalRedirecionarManutencao implements OnInit {
+  @Output() fechar = new EventEmitter<void>();
 
+  funcionarios: any[] = [];
+  funcionarioSelecionadoId: number | null = null;
+
+  private todosOsFuncionarios = [
+    { id: 1, nome: 'Maria' },
+    { id: 2, nome: 'Mário' },
+    { id: 3, nome: 'Carlos' }
+  ];
+
+  private idFuncionarioLogado = 1;
+
+  ngOnInit(): void {
+    this.funcionarios = this.todosOsFuncionarios.filter(f => f.id !== this.idFuncionarioLogado);
+  }
+
+  onRedirecionar(): void {
+    if (!this.funcionarioSelecionadoId) {
+      alert('Por favor, selecione um funcionário.');
+      return;
+    }
+    const funcionarioDestino = this.todosOsFuncionarios.find(f => f.id === this.funcionarioSelecionadoId);
+    console.log(`Manutenção redirecionada para: ${funcionarioDestino?.nome}`);
+
+    alert(`Serviço redirecionado com sucesso para ${funcionarioDestino?.nome}!`);
+    this.fechar.emit();
+  }
+
+  onCancelar(): void {
+    this.fechar.emit();
+  }
 }
