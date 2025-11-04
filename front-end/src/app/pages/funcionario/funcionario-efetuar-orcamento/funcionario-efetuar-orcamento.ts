c12f01898443e71cebd6b3901009b92ff15c31c0
Spirou1 <enzospironellipinheiro@gmail.com>
2025-11-04 01:19:43 -0300

    comecando a integrar orcamentos

--- front-end/src/app/pages/funcionario/funcionario-efetuar-orcamento/funcionario-efetuar-orcamento.ts
+++ front-end/src/app/pages/funcionario/funcionario-efetuar-orcamento/funcionario-efetuar-orcamento.ts
@@ -1,50 +1,67 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { StorageService } from '../../../services/storage-service';
 import { Solicitacao } from '../../../models/solicitacao';
 import { Usuario } from '../../../models/usuario';
 import { OrcamentoService } from '../../../services/orcamento-service';
 import { FormsModule } from '@angular/forms';
+import { SolicitacaoService } from '../../../services/solicitacao-service';
+import { SpinnerComponent } from '../../../shared/loading-spinner/spinner';
+import { EstadosSolicitacao, translateEstado } from '../../../models/enums/estados-solicitacao';
 
 @Component({
   selector: 'app-funcionario-efetuar-orcamento',
   standalone: true,
-  imports: [CommonModule, DatePipe, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, SpinnerComponent],
   templateUrl: './funcionario-efetuar-orcamento.html',
   styleUrl: './funcionario-efetuar-orcamento.css'
 })
 export class FuncionarioEfetuarOrcamento implements OnInit {
 
-  solicitacao: Solicitacao | undefined;
+  solicitacao: Solicitacao | null = null;
   cliente: Usuario | undefined;
   valorOrcamento: number | undefined;
+  descricaoOrcamento: string | undefined;
+  isLoading: boolean = false;
 
 
-  private readonly SOLICITACOES_KEY = 'solicitacoes';
-  private readonly USUARIOS_KEY = 'usuarios';
-
-  constructor(private route: ActivatedRoute, private router: Router, private storageService: StorageService, private orcamentoService: OrcamentoService) {}
+  constructor(private route: ActivatedRoute, private router: Router, private orcamentoService: OrcamentoService, private solicitacaoService: SolicitacaoService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     const idDaUrl = this.route.snapshot.paramMap.get('id');
-    const idNumerico = idDaUrl ? +idDaUrl : 0;
-
-    const todasAsSolicitacoes = this.storageService.getDados(this.SOLICITACOES_KEY);
-    const todosOsUsuarios = this.storageService.getDados(this.USUARIOS_KEY);
-
-    this.solicitacao = todasAsSolicitacoes.find(
-      (s: any) => s.id === idNumerico
-    );
-
-    if (this.solicitacao && todosOsUsuarios) {
-        this.solicitacao.dataCriacao = new Date(this.solicitacao.dataCriacao).toISOString();
-
-        this.cliente = todosOsUsuarios.find(
-            (u: any) => u.id === this.solicitacao?.usuario.id
-        );
+    
+    if (!idDaUrl) {
+      console.error('ID da solicitação não encontrado na URL');
+      return;
     }
+
+    const idNumerico = +idDaUrl;
+    
+    this.buscarPorId(idNumerico);
+
+    this.cdr.detectChanges();
   }
 
+  traduzirEstado(estd: EstadosSolicitacao): string {
+    return translateEstado(estd);
+  }
 
-}
+  buscarPorId(id: number): void {
+    this.isLoading = true;
+    this.solicitacaoService.buscarPorId(id).subscribe({
+      next: (data) => {
+        this.solicitacao = data;
+        this.isLoading = false;
+        this.cdr.detectChanges(); 
+      },
+      error: (error) => {
+        console.error('Erro ao carregar solicitações:', error);
+      },
+    });
+  }
+
+    efetuarOrcamento(): void {}
+
+  }
+
+  
--- front-end/src/app/services/orcamento-service.ts
+++ front-end/src/app/services/orcamento-service.ts
@@ -2,10 +2,18 @@ import { Injectable } from '@angular/core';
 import { StorageService } from './storage-service';
 import { Solicitacao } from '../models/solicitacao';
 import { Usuario } from '../models/usuario';
+import { ToastService } from './toast-service';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { API_URL, APIResponse } from '../../api/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrcamentoService {
   
+
+  constructor(private toastService: ToastService, private httpClient: HttpClient) { }
+
+
 }
