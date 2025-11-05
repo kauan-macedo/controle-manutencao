fa2e6954a59d86804e34a9702e9e102c7e317046
victorbetoni <victorbetoni@protonmail.com>
2025-11-05 09:42:21 -0300

    .

--- backend/src/main/java/com/controlemanutencao/model/LogSolicitacao.java
+++ backend/src/main/java/com/controlemanutencao/model/LogSolicitacao.java
@@ -3,38 +3,38 @@ package com.controlemanutencao.model;
 import com.controlemanutencao.repository.Converter;
 import com.controlemanutencao.model.enums.StatusSolicitacao;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "ME_LogSolicitacao")
-@NoArgsConstructor
 public class LogSolicitacao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "IdLogSolicitacao", columnDefinition="INT")
-    @Getter private Long id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "IdSolicitacao")
-    @Getter @Setter private Solicitacao solicitacao;
+    private Solicitacao solicitacao;
 
     @ManyToOne
     @JoinColumn(name = "Agente")
-    @Getter @Setter private Usuario agente;
+    private Usuario agente;
 
     @Column(name = "StatusAnterior")
     @Convert(converter = Converter.StatusSolicitacaoConverter.class)
-    @Getter @Setter private StatusSolicitacao statusAnterior;
+    private StatusSolicitacao statusAnterior;
 
     @Column(name = "NovoStatus")
     @Convert(converter = Converter.StatusSolicitacaoConverter.class)
-    @Getter @Setter private StatusSolicitacao novoStatus;
+    private StatusSolicitacao novoStatus;
 
     @Column(name = "Data")
-    @Getter @Setter private Long data;
+    private Long data;
+
+
+    public LogSolicitacao() {
+    }
 
     public LogSolicitacao(Solicitacao s, StatusSolicitacao statusAnterior, StatusSolicitacao novoStatus, Long data) {
         this.statusAnterior = statusAnterior;
@@ -43,4 +43,27 @@ public class LogSolicitacao {
         this.solicitacao = s;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public Solicitacao getSolicitacao() {
+        return solicitacao;
+    }
+
+    public Usuario getAgente() {
+        return agente;
+    }
+
+    public StatusSolicitacao getStatusAnterior() {
+        return statusAnterior;
+    }
+
+    public StatusSolicitacao getNovoStatus() {
+        return novoStatus;
+    }
+
+    public Long getData() {
+        return data;
+    }
 }
