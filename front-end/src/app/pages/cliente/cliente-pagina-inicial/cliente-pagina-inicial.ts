73460c610d22742c465a30e5c3282758a58b4996
Spirou1 <enzospironellipinheiro@gmail.com>
2025-11-07 21:36:51 -0300

    substituindo componente de loading no perfil de cliente

--- front-end/src/app/pages/cliente/cliente-pagina-inicial/cliente-pagina-inicial.ts
+++ front-end/src/app/pages/cliente/cliente-pagina-inicial/cliente-pagina-inicial.ts
@@ -7,8 +7,8 @@ import { SolicitacaoService } from '../../../services/solicitacao-service';
 import { ToastComponent } from '../../../shared/toast-component/toast-component';
 import { ToastService } from '../../../services/toast-service';
 import { EstadosSolicitacao, translateEstado ,} from '../../../models/enums/estados-solicitacao';
-import { SpinnerComponent } from '../../../shared/loading-spinner/spinner';
 import { formataData } from '../../../utils/utils';
+import { LoadingOverlayComponent } from '../../../shared/loading-overlay.component';
 
 @Component({
   selector: 'app-cliente-pagina-inicial',
@@ -17,7 +17,7 @@ import { formataData } from '../../../utils/utils';
     CommonModule,
     RouterModule,
     ToastComponent,
-    SpinnerComponent
+    LoadingOverlayComponent
   ],
   templateUrl: './cliente-pagina-inicial.html',
   styleUrl: './cliente-pagina-inicial.css',
@@ -26,32 +26,41 @@ export class ClientePaginaInicial implements OnInit {
   formataData = formataData
   exibirModal: boolean = false;
   minhasSolicitacoes: Solicitacao[] = [];
-  //variavel para exibir o componente de loading
-  isLoading: boolean = false;
+  loading = false;
+ 
 
   constructor(private solicitacaoService: SolicitacaoService, private toastService: ToastService, private cdr: ChangeDetectorRef) {}
 
+  ngOnInit(): void {
+    this.carregarSolicitacoes();
+  }
+
+  endLoad = () => {
+    setTimeout(() => {
+        this.loading = false;
+        this.cdr.detectChanges();
+      })
+  }
+
   //deixando essa funcao fora do ngoninit para poder chamar ela ao fechar o modal
   carregarSolicitacoes(): void {
-    this.isLoading = true;
+    this.loading = true;
     this.solicitacaoService.buscarTodas(-1, false, null, null).subscribe({
       next: (solicitacoes) => {
         this.minhasSolicitacoes = solicitacoes;
-        this.isLoading = false;
+       
         this.cdr.detectChanges();
       },
       error: (error) => {
         console.error('Erro ao carregar solicitações:', error);
         this.toastService.showError('Erro ao carregar solicitações.');
-        this.isLoading = false;
       },
+      complete: () => {
+        this.endLoad()
+      }
     });
 
     console.log()
-  }
-
-  ngOnInit(): void {
-    this.carregarSolicitacoes();
   }
 
   abrirModal(): void {
